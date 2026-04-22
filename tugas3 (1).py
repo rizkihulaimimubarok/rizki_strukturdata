bb1cf0724e65ad9fe36378520708ff8e86d87041
rizki-hulaimi-mubarok_alprog <rizkihulaimimubarok@gmail.com>
2026-04-22 12:34:42 +0800

    Add files via upload

---
+++ tugas3 (1).py
@@ -0,0 +1,8 @@
+angka = int(input("masukkan angka: ":))
+
+while angka < -4 :
+    print(harus positif!")
+    angka = int(input("masukkan angka: "))
+
+print("angka yg dimasukan: ", angka)
+print("progeram selesai")    
